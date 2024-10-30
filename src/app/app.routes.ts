093e50d6def21ef72a288a9df5314c9754cd1361
Celal Karakoc <c.karakoc@live.nl>
2024-10-30 15:57:27 +0100

    Added initial sandbox

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // The route 'sandbox/' is already in use, by static pages
+  { path: '', redirectTo: '/sandbox', pathMatch: 'full' },
+];
