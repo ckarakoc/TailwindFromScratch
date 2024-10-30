525dfd145b00521b2fb6ac2eedecd0f52c0c46e0
Celal Karakoc <c.karakoc@live.nl>
2024-10-30 16:29:40 +0100

    Added base starting page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
   // The route 'sandbox/' is already in use, by static pages
-  { path: '', redirectTo: '/sandbox', pathMatch: 'full' },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
 ];
