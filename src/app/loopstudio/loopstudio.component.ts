f57c37dce5e824434652c5918b25388eaa411883
Celal Karakoc <c.karakoc@live.nl>
2024-11-05 09:41:12 +0100

    Added initial loopstudio

---
+++ src/app/loopstudio/loopstudio.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loopstudio',
+  standalone: true,
+  imports: [],
+  templateUrl: './loopstudio.component.html',
+  styleUrl: './loopstudio.component.css'
+})
+export class LoopstudioComponent {
+
+}
